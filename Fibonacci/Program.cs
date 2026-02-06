1e19dfded4d8429586bed9abcae0cc7db60f3b63
teymouri86 <fatemeh.teymoori1020@gmail.com>
2026-02-06 17:42:34 +0330

    upload fibonacci project

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+
+
+
+        // متد بازگشتی برای فیبوناچی
+        static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            if (n == 1)
+                return 1;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("یک عدد وارد کنید: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nسری فیبوناچی تا عدد " + n + ":");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
